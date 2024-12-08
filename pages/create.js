86c776eb7ad390c5485d3e6da70f9460651487cb
Nick <nrodrig1@gmail.com>
2024-12-08 17:08:41 +0100

    create page with fetch

--- pages/create.js
+++ pages/create.js
@@ -3,21 +3,16 @@ import TemplateView from "../components/TemplateView";
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlus, faCircleMinus } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch, useSelector } from "react-redux";
 
 function Create() {
+  const user = useSelector((state) => state.user.value);
   const [selectedRadioNginx, selectedRadioNginxSetter] = useState("");
   const [selectedRadioFramework, selectedRadioFrameworkSetter] = useState("");
   const [serverNames, serverNamesSetter] = useState([""]);
   const [cwd, cwdSetter] = useState("");
   const [localIp, localIpSetter] = useState("");
   const [port, portSetter] = useState("");
-
-  // const handleRadioNginx = (event) => {
-  //   selectedRadioNginxSetter(event.target.value);
-  // };
-  // const handleRadioFramework = (event) => {
-  //   selectedRadioFrameworkSetter(event.target.value);
-  // };
 
   const handleServerNameChange = (index, value) => {
     const updatedServerNames = [...serverNames];
@@ -33,11 +28,69 @@ function Create() {
     serverNamesSetter(updatedServerNames);
   };
 
-  const btnCreateFile = () => {
+  // Determine if the div should be visible
+  const showCwd =
+    selectedRadioNginx === "Confd" &&
+    (selectedRadioFramework === "NextJs" ||
+      selectedRadioFramework === "PythonFlask");
+
+  const btnCreateFile = async () => {
+    let serverNamesStringCommaSeparated = serverNames.join(",");
+
+    console.log(`sending to :::`);
+    console.log(
+      `${user.currentMachineDisplay.urlFor404Api}/create/server-file`
+    );
     console.log(`selectedRadioNginx: ${selectedRadioNginx}`);
     console.log(`selectedRadioFramework: ${selectedRadioFramework}`);
-    console.log(`serverNames: ${serverNames}`);
+    console.log(
+      `serverNamesStringCommaSeparated: ${serverNamesStringCommaSeparated}`
+    );
     console.log(`cwd: ${cwd}`);
+
+    let reqBodyObj = {
+      framework: selectedRadioFramework,
+      nginxDir: selectedRadioNginx,
+      serverNames: serverNamesStringCommaSeparated,
+      port: port,
+    };
+    if (cwd) {
+      reqBodyObj = { ...reqBodyObj, appCwd: cwd };
+    }
+    if (localIp) {
+      reqBodyObj = { ...reqBodyObj, localIp: localIp };
+    }
+    console.log(
+      `---> what are serverNaems: ${serverNamesStringCommaSeparated}`
+    );
+
+    const response = await fetch(
+      `${user.currentMachineDisplay.urlFor404Api}/create/server-file`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${user.token}`, // Add token to Authorization header
+        },
+        body: JSON.stringify(reqBodyObj),
+      }
+    );
+    if (response.status == 200) {
+      const resJson = await response.json();
+
+      selectedRadioNginxSetter("");
+      selectedRadioFrameworkSetter("");
+      serverNamesSetter([""]);
+      cwdSetter("");
+      localIpSetter("");
+      portSetter("");
+      window.alert(`Succsess! ${resJson.message}`);
+    } else if (response.status == 401) {
+      const resJson = await response.json();
+      window.alert(`Error 401 because: ${resJson.error}`);
+    } else {
+      window.alert(`There was a server error: ${response.status}`);
+    }
   };
 
   return (
@@ -48,7 +101,14 @@ function Create() {
             <span className={`${styles.spanLabel}`}>Nginx dir: </span>
             <span className={styles.spanRadio}>
               <form className={styles.form}>
-                <label className={styles.labelRadio}>
+                {/* <label className={styles.labelRadio}> */}
+                <label
+                  className={
+                    selectedRadioNginx === "Confd"
+                      ? styles.labelRadioSelected
+                      : styles.labelRadio
+                  }
+                >
                   <input
                     name="nginxDir"
                     type="radio"
@@ -59,7 +119,13 @@ function Create() {
                   />
                   conf.d
                 </label>
-                <label className={styles.labelRadio}>
+                <label
+                  className={
+                    selectedRadioNginx === "SitesAvailable"
+                      ? styles.labelRadioSelected
+                      : styles.labelRadio
+                  }
+                >
                   <input
                     name="nginxDir"
                     type="radio"
@@ -77,7 +143,13 @@ function Create() {
             <span className={styles.spanLabel}>Framework: </span>
             <span className={styles.spanRadio}>
               <form className={styles.form}>
-                <label className={styles.labelRadio}>
+                <label
+                  className={
+                    selectedRadioFramework === "expressJs"
+                      ? styles.labelRadioSelected
+                      : styles.labelRadio
+                  }
+                >
                   <input
                     name="framework"
                     type="radio"
@@ -89,7 +161,13 @@ function Create() {
                   />
                   ExpressJS
                 </label>
-                <label className={styles.labelRadio}>
+                <label
+                  className={
+                    selectedRadioFramework === "NextJs"
+                      ? styles.labelRadioSelected
+                      : styles.labelRadio
+                  }
+                >
                   <input
                     name="framework"
                     type="radio"
@@ -101,7 +179,13 @@ function Create() {
                   />
                   Next.JS
                 </label>
-                <label className={styles.labelRadio}>
+                <label
+                  className={
+                    selectedRadioFramework === "PythonFlask"
+                      ? styles.labelRadioSelected
+                      : styles.labelRadio
+                  }
+                >
                   <input
                     name="framework"
                     type="radio"
@@ -120,7 +204,7 @@ function Create() {
 
           <div className={styles.divInputGroupText}>
             <span className={`${styles.spanLabel} ${styles.spanLabelServer}`}>
-              server names:{" "}
+              <div>server names: </div>
             </span>
             <span className={styles.spanInput}>
               <div className={styles.divInputServerNames}>
@@ -134,12 +218,18 @@ function Create() {
                       value={name}
                       placeholder="server name"
                     />
+
                     {index > 0 && (
                       <FontAwesomeIcon
                         icon={faCircleMinus}
                         onClick={() => btnRemoveServerNameInput(index)}
                         className={styles.iconMinus}
                       />
+                    )}
+                    {(name.includes(" ") || name.includes(",")) && (
+                      <p className={styles.errorText}>
+                        Server names cannot contain spaces or commas.
+                      </p>
                     )}
                   </div>
                 ))}
@@ -155,46 +245,49 @@ function Create() {
             </span>
           </div>
           {/* ---- DIV - Server Name ----- */}
-
-          <div className={styles.divInputGroupText}>
-            <span className={`${styles.spanLabel} ${styles.spanLabelText}`}>
-              CWD:{" "}
-            </span>
-            <span className={styles.spanInput}>
-              <div className={styles.divInputText}>
-                <input
-                  className={styles.inputServerName}
-                  onChange={
-                    (e) => cwdSetter(e.target.value)
-                    // handleServerNameChange(index, e.target.value)
-                  }
-                  value={cwd}
-                  placeholder="/home/user/application/appName"
-                />
-              </div>
-            </span>
-          </div>
+          {showCwd && (
+            <div className={styles.divInputGroupText}>
+              <span className={`${styles.spanLabel} ${styles.spanLabelText}`}>
+                CWD:{" "}
+              </span>
+              <span className={styles.spanInput}>
+                <div className={styles.divInputText}>
+                  <input
+                    className={styles.inputServerName}
+                    onChange={
+                      (e) => cwdSetter(e.target.value)
+                      // handleServerNameChange(index, e.target.value)
+                    }
+                    value={cwd}
+                    placeholder="/home/user/application/appName"
+                  />
+                </div>
+              </span>
+            </div>
+          )}
           {/* ---- small DIV - Current Working Dir  ----- */}
-
-          <div className={styles.divInputGroupText}>
-            <span className={`${styles.spanLabel} ${styles.spanLabelText}`}>
-              Local IP:{" "}
-            </span>
-            <span className={styles.spanInput}>
-              <div className={styles.divInputText}>
-                <input
-                  className={styles.inputServerName}
-                  onChange={
-                    (e) => localIpSetter(e.target.value)
-                    // handleServerNameChange(index, e.target.value)
-                  }
-                  value={localIp}
-                  placeholder="192.0.0.0"
-                />
-              </div>
-            </span>
-          </div>
+          {selectedRadioNginx === "SitesAvailable" && (
+            <div className={styles.divInputGroupText}>
+              <span className={`${styles.spanLabel} ${styles.spanLabelText}`}>
+                Local IP:{" "}
+              </span>
+              <span className={styles.spanInput}>
+                <div className={styles.divInputText}>
+                  <input
+                    className={styles.inputServerName}
+                    onChange={
+                      (e) => localIpSetter(e.target.value)
+                      // handleServerNameChange(index, e.target.value)
+                    }
+                    value={localIp}
+                    placeholder="192.0.0.0"
+                  />
+                </div>
+              </span>
+            </div>
+          )}
           {/* ---- small DIV - Local IP  ----- */}
+
           <div className={styles.divInputGroupText}>
             <span className={`${styles.spanLabel} ${styles.spanLabelText}`}>
               Port:{" "}
@@ -203,17 +296,34 @@ function Create() {
               <div className={styles.divInputText}>
                 <input
                   className={styles.inputServerName}
-                  onChange={
-                    (e) => portSetter(e.target.value)
-                    // handleServerNameChange(index, e.target.value)
-                  }
+                  onChange={(e) => portSetter(e.target.value)}
                   value={port}
                   placeholder="8000"
                 />
               </div>
             </span>
           </div>
+
           {/* ---- small DIV - Port  ----- */}
+
+          {/* ---- Conditional Div ----
+          {showConditionalDiv && (
+            <div
+              className={styles.conditionalDiv}
+              style={{
+                position: "absolute",
+                bottom: 0,
+                width: "100%",
+                backgroundColor: "#23263A",
+                color: "white",
+                padding: "1rem",
+                textAlign: "center",
+              }}
+            >
+              This is a conditional div that appears when `conf.d` and either
+              `Next.JS` or `Python Flask` are selected!
+            </div>
+          )} */}
 
           <div className={styles.divBtnCreateFile}>
             <button
